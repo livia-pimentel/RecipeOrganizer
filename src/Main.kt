fef7273f5550a9a6b24c0e6ceab0de8f85b9d276
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-03-24 14:32:02 -0300

    Test add recipe

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,42 @@
+import java.io.DataInput
+
 fun main() {
     val recipeManagement = RecipeManagement("src/recipes.json")
 
-    println("Existing Recipes:")
+//    println("Existing Recipes:")
+//    recipeManagement.getAllRecipes().forEach { recipe ->
+//        println("Name: ${recipe.name}")
+//        println("Ingredients: ")
+//        recipe.ingredients.forEach { ingredients, quantaty ->
+//            println("- $ingredients: $quantaty")
+//        }
+//        println("Instructions: ${recipe.instructions}")
+//        println("Category: ${recipe.category}")
+//        println("--------------------")
+//    }
+
+    println("\n Enter details for the new recipe")
+    print("Name: ")
+    val name = readLine() ?: ""
+    print("Ingredients (Ingredient: Quantity): ")
+    val ingredientsInput = readLine() ?: ""
+    print("Instructions: ")
+    val instructions = readLine() ?: ""
+    print("category: ")
+    val category = readLine() ?: ""
+
+    // Pass ingredientsInput as String to addRecipe
+    recipeManagement.addRecipe(name, ingredientsInput, instructions, category)
+
+    println("\nUpdate Recipes")
     recipeManagement.getAllRecipes().forEach { recipe ->
         println("Name: ${recipe.name}")
         println("Ingredients: ")
-        recipe.ingredients.forEach { ingredients, quantaty ->
-            println("- $ingredients: $quantaty")
+        recipe.ingredients.forEach { ingredient, quantity ->
+            println("- $ingredient: $quantity")
         }
         println("Instructions: ${recipe.instructions}")
-        println("Category: ${recipe.category}")
+        println("category: ${recipe.category}")
         println("--------------------")
     }
-
 }
