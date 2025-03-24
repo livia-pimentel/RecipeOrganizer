bcc77f8de0cffb88b077fe7ddd38193dc3995450
Livia Pimentel <livia_macedo89@yahoo.com.br>
2025-03-24 09:56:28 -0300

    Test loadRecipes function

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
+    val recipeManagement = RecipeManagement("src/recipes.json")
+
+    println("Existing Recipes:")
+    recipeManagement.getAllRecipes().forEach { recipe ->
+        println("Name: ${recipe.name}")
+        println("Ingredients: ")
+        recipe.ingredients.forEach { ingredients, quantaty ->
+            println("- $ingredients: $quantaty")
+        }
+        println("Instructions: ${recipe.instructions}")
+        println("Category: ${recipe.category}")
+        println("--------------------")
+    }
 
 }
